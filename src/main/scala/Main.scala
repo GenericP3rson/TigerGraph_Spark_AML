dd2e2b11cd2afd5d8846e2f06fb1fb49e8c65ab9
Shreya <genericp3rson@gmail.com>
2022-06-27 06:46:24 -0500

    Basic, functional TG/Spark Connector

---
+++ src/main/scala/Main.scala
@@ -0,0 +1,34 @@
+import org.apache.spark.sql.SparkSession
+
+object Main {
+  def main(args: Array[String]) {
+     val spark = SparkSession.builder.appName("Simple Application").getOrCreate()
+
+      // read vertex
+      val jdbcDF1 = spark.read.format("jdbc").options(
+        Map(
+          "driver" -> "com.tigergraph.jdbc.Driver",
+          "url" -> "jdbc:tg:http://localhost:14240",
+          "username" -> "tigergraph",
+          "password" -> "tigergraph",
+          "graph" -> "socialNet", // graph name
+          "dbtable" -> "vertex post", // vertex type
+          "limit" -> "10", // number of vertices to retrieve
+          "debug" -> "0")).load()
+      println(jdbcDF1.show())
+
+      // read edge
+      val jdbcDF2 = spark.read.format("jdbc").options(
+        Map(
+          "driver" -> "com.tigergraph.jdbc.Driver",
+          "url" -> "jdbc:tg:http://127.0.0.1:14240",
+          "username" -> "tigergraph",
+          "password" -> "tigergraph",
+          "graph" -> "socialNet", // graph name
+          "dbtable" -> "edge friend", // edge type
+          "limit" -> "10", // number of edges to retrieve
+          "source" -> "person2", // source vertex id
+          "debug" -> "0")).load()
+      println(jdbcDF2.show())
+  }
+}
